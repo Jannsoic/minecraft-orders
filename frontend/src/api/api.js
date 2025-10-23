76dcd4f242c43eff6f754482d12dec506a1e2fd0
Jannsoic <jannicschmalle@gmail.com>
2025-10-23 14:12:11 +0200

    Refactor API base URL initialization

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-// Base URL dynamisch aus Environment Variable (VITE_API_BASE)
-const API = axios.create({ baseURL: import.meta.env.VITE_API_BASE + '/api' });
+const API = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE + '/api'
+});
 
-// Funktion um Token zu setzen
 export function setToken(token){
   API.defaults.headers.common['Authorization'] = 'Bearer ' + token;
 }
